4c7d69a2ed015f0ecc931bf8052abbcbeb1c84a3
adaau <ada.au.cheng@gmail.com>
2015-09-27 10:17:53 +0800

    completed MPG without else if and completed BMI basic function

--- starter-code/scripts/app.js
+++ starter-code/scripts/app.js
@@ -1,5 +1,4 @@
 window.onload = function () {
-/* BASIC CALC */
 function getValue(id) {
   return document.getElementById(id).value;
 }
@@ -8,9 +7,15 @@ function getFloat(id) {
   return parseFloat(getValue(id));
 }
 
+function setTo2DP(id) {
+  return Math.round(id * 100) / 100;
+}
+
 function setHtml(id, html) {
   document.getElementById(id).innerHTML = html;
 }
+
+/* BASIC CALC */
 
 function basicCalc() {
   var num1 = getFloat("basic-num-1");
@@ -35,14 +40,72 @@ function basicCalc() {
   	  console.log("Check")
   }
 
-  var message = num1 + " " + oper + " " + num2 + " = " + ans;
-  setHtml("basic-answer-alert", message)
+  ans = setTo2DP(ans);
+  var basicMessage = num1 + " " + oper + " " + num2 + " = " + ans;
+  setHtml("basic-answer-alert", basicMessage)
 }
 
 document.getElementById("basic-calc").addEventListener("click", basicCalc);
 
 
-/* TRIP CALC */
+/* TRIP COST CALC */
+function tripCalc() {
+  var dist = getFloat("trip-distance");
+  var mpg = getFloat("trip-mpg");
+  var cost = getFloat("trip-cost");
+  var speed = getFloat("trip-speed");
+  var ans = null;
+
+/* calculating trip cost */
+/* formula for cost calculation for speeds > 60 did not work */
+  costAns = dist / mpg * cost;
+  costAns = setTo2DP(costAns);
+
+/* calculating trip time */
+  timeAns = dist / speed;
+  timeAns = setTo2DP(timeAns);
+
+  var tripMessage = "The trip will cost " + costAns + " and it will take " + timeAns + " hours.";
+
+  setHtml("trip-answer-alert", tripMessage);
+}
+
+document.getElementById("trip-calc").addEventListener("click", tripCalc);
+
+
+/* BMI CALC */
+function bmiCalc() {
+  var mass = getFloat("bmi-mass");
+  var height = getFloat("bmi-height");
+
+  bmiAns = mass / Math.pow(height, 2);
+  bmiAns = setTo2DP(bmiAns);
+
+  var bmiMessage = "Your BMI is " + bmiAns;
+
+  setHtml("bmi-answer-alert", bmiMessage);
+}
+
+document.getElementById("bmi-calc").addEventListener("click", bmiCalc);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
